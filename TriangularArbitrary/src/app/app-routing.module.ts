f24fff7bc3901003ec603c49826f44060a5216a6
merg13 <mitchellergen@gmail.com>
2020-10-12 12:50:24 -0500

    misc updates; favicon; background; styles

--- TriangularArbitrary/src/app/app-routing.module.ts
+++ TriangularArbitrary/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { CreateTicketComponent } from './create-ticket/create-ticket.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TopTenComponent } from './top-ten/top-ten.component';
+import { ConversionComponent } from './conversion/conversion.component';
+import { AdminAccountManagementComponent } from './admin-account-management/admin-account-management.component';
 
 const routes: Routes = [
   { path: 'create-ticket', component: CreateTicketComponent },
   // { path: 'favorites', component: FavoriteComponent },
   // { path: 'search', component: SearchComponent },
-  // { path: 'conversion', component: ConversionComponent },
-  // { path: 'admin-account-management', component: AdminAccountManagementComponent },
+  { path: 'conversion', component: ConversionComponent },
+  { path: 'admin', component: AdminAccountManagementComponent },
+  { path: 'top-ten', component: TopTenComponent },
   // { path: '', component: FavoriteComponent },
 ];
 
