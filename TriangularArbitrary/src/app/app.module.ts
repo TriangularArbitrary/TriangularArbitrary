e7c1f0be9081b0423f0aca35166efb733f062241
merg13 <mitchellergen@gmail.com>
2020-10-13 08:28:36 -0500

    add routerlink-favorites and search; module tweak

--- TriangularArbitrary/src/app/app.module.ts
+++ TriangularArbitrary/src/app/app.module.ts
@@ -4,18 +4,23 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateTicketComponent } from './create-ticket/create-ticket.component';
 import { AdminAccountManagementComponent } from './admin-account-management/admin-account-management.component';
 import { ConversionComponent } from './conversion/conversion.component';
 import { TopTenComponent } from './top-ten/top-ten.component';
+import { SearchComponent } from './search/search.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateTicketComponent,
     AdminAccountManagementComponent,
+    SearchComponent,
+    FavoritesComponent,
     ConversionComponent,
     TopTenComponent
   ],
