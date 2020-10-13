5821452d87b288681ea7f771416488dbf4377357
kvnlineback <49303204+kvnlineback@users.noreply.github.com>
2020-10-13 06:40:23 -0600

    Delete ITicketModel.ts

--- Models/ITicketModel.ts
+++
@@ -1,8 +0,0 @@
-import { TicketTypes, TicketSeverityTypes } from '../Enums/Enums';
-
-export class ITicketModel {
-  subject: string;
-  type: TicketTypes;
-  severity: TicketSeverityTypes;
-  ticketReason: string;
-}
