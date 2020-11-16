db45ebe8cc078d933ed4229db2b3195d4df62fc8
kvnlineback <49303204+kvnlineback@users.noreply.github.com>
2020-11-16 00:04:28 -0700

    Add files via upload

--- TriangularArbitrary/src/app/favorites/favorites.component.ts
+++ TriangularArbitrary/src/app/favorites/favorites.component.ts
@@ -1,64 +1,88 @@
-import { Component, OnInit } from '@angular/core';
-import { LocalStorageKeys } from '../Enums/Enums';
+import { Component, Input, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { Stock } from '../Models/Stock';
+import { AccountService } from '../Services/account.service';
 import { AlphaVantageService } from '../Services/alpha-vantage.service';
+import { FavoritesStorageService } from '../Services/favorites-storage.service';
+
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
-  styleUrls: ['./favorites.component.css']
+  styleUrls: ['./favorites.component.css'],
 })
 export class FavoritesComponent implements OnInit {
 
   stocks: Stock[] = [];
-  favs = JSON.parse(localStorage.getItem(LocalStorageKeys.Favorites)) as string[];
+  textColor: string;
 
-  constructor(private serv: AlphaVantageService) { }
+  constructor(
+    private serv: AlphaVantageService,
+    private favServ: FavoritesStorageService,
+    private toastr: ToastrService,
+    private accServ: AccountService
+   ) {
+
+  }
 
   ngOnInit(): void {
     this.loadFavorites();
   }
 
-
-  loadFavorites() {
-    let currentFavs: string[] = JSON.parse(localStorage.getItem(LocalStorageKeys.Favorites));
-    if(currentFavs != null) {
-      this.favs = currentFavs;
-      for(var i = 0; i<currentFavs.length; i++) {
-        this.retrieveStock(currentFavs[i]);
+  async loadFavorites() {
+    let currentFavs = await this.favServ.getAllFavorites(this.accServ.getUserAccount().email);
+    for(let i = 0; i < currentFavs.length; i++) {
+      this.retrieveStock(currentFavs[i]);
     }
-   }
   }
 
   refreshData() {
     let copy: Stock[] = this.stocks;
     this.stocks = [];
-    for(var i = 0; i<copy.length; i++) {
+    for (var i = 0; i < copy.length; i++) {
       let stock: Stock = copy[i];
       this.retrieveStock(stock.symbol);
     }
   }
 
   retrieveStock(symbol: string) {
-    console.log(symbol);
-    this.serv.getStocks(symbol).subscribe
-    (
-      (response)=>
-      {
+    this.serv.getStocks(symbol).subscribe(
+      (response) => {
         let data = response['Global Quote'];
-        let newStock = new Stock(data['01. symbol'],data['02. open'],data['03. high'], data['04. low'],data['05. price'],data['06. volume'],data['07. latest trading day']
-        ,data['08. previous close'], data['09. change'],data['10. change percent']);
+        let change = data['09. change'];
+        let newStock = new Stock(
+          data['01. symbol'],
+          data['02. open'],
+          data['03. high'],
+          data['04. low'],
+          data['05. price'],
+          data['06. volume'],
+          data['07. latest trading day'],
+          data['08. previous close'],
+          change,
+          data['10. change percent']
+        );
         this.stocks.push(newStock);
       },
       (error) => console.log(error)
-    )
+    );
   }
 
   deleteFavorite(stock: Stock) {
-    this.stocks.splice(this.stocks.indexOf(stock), 1);
-    this.favs.splice(this.favs.indexOf(stock.symbol), 1);
-    localStorage.setItem(LocalStorageKeys.Favorites, JSON.stringify(this.favs));
-    alert("deleted " + stock.symbol + " from favorites")
+    if(confirm("Delete " + stock.symbol + " from favorites?")) {
+      this.stocks.splice(this.stocks.indexOf(stock), 1);
+      this.favServ.deleteFavorite(stock.symbol, this.accServ.getUserAccount().email);
+      this.toastr.info(stock.symbol + " deleted from favorites");
+    }
+
   }
 
+  getTextColor(index: number): string {
+    let stock = this.stocks[index];
+    if (parseFloat(stock.change) < 0) {
+      return 'red';
+    } else {
+      return 'green';
+    }
+  }
 }
